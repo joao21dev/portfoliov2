87fa02a681375613904c04d44fe5857ee5e9591d
joao21dev <joao21dev@gmail.com>
2022-04-02 13:13:56 -0300

    portfolio v2 react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar";
+import styled from "styled-components";
+import "./App.css";
+import About from "./About";
+import Projects from "./Projects";
+import Education from "./Education";
+import Experience from "./Experience";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Navbar></Navbar>
+      <About />
+      <Projects />
+      <Education />
+      <Experience />
+      <Footer />
+    </Container>
   );
 }
+
+export const Container = styled.div`
+  background-color: #f9f6ee;
+  height: 100%;
+`;
 
 export default App;
